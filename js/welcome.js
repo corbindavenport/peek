c5d16fbdf41c9c1242e23260b90edef724372564
Corbin Davenport <me@corbin.io>
2023-03-08 23:30:47 -0500

    Update to Bootstrap 5.3, update welcome/settings

--- js/welcome.js
+++ js/welcome.js
@@ -1,24 +1,2 @@
 // Add version number to welcome page
 document.querySelector(".version").innerHTML = chrome.runtime.getManifest().version
-
-// Link buttons
-document.querySelectorAll('.link-btn').forEach(function (el) {
-	el.addEventListener('click', function () {
-		chrome.tabs.create({ url: el.getAttribute('data-url') })
-	})
-})
-
-// Show credits
-fetch('https://corbin.io/supporters.json').then(function (response) {
-	response.json().then(function (data) {
-		var creditsList = 'Diamond supporters: '
-		for (var i = 0; i < data['supporters'].length; i++) {
-			creditsList += data['supporters'][i] + ', '
-		}
-		creditsList = creditsList.substring(0, creditsList.length - 2)
-		document.getElementById('peek-credits').innerText = creditsList
-	})
-})
-.catch(function (err) {
-	document.getElementById('peek-credits').innerText = 'There was an error fetching Peek supporters.'
-})
